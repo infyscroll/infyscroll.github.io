ce78787bde46930c733bc5e8617a9e6562ae31f9
infyscroll <70573082+infyscroll@users.noreply.github.com>
2022-05-20 13:21:41 -0400

    Add files via upload

---
+++ .github/actions/fetch-json-write-file/index.js
@@ -0,0 +1,35 @@
+/**
+ * Index JS (Uncompiled)
+ *
+ * To compile this script into dist/index.js with all the necessary libraries:
+ * 1. install vercel/ncc: npm i -g @vercel/ncc
+ * 2. build index.js: ncc build index.js --license licenses.txt
+ * @see https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github 
+ */
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+import fetch from 'node-fetch';
+
+(async () => {
+  let successful = false;
+  try {
+    const url = core.getInput("url");
+    const response = await fetch(url);
+    const json = await response.json();
+    const filter = json.filter(d => d.data && d.data.url && d.data.nextLink && d.data.pageElement);
+    if (filter && filter.length > 0) {
+      const text = JSON.stringify(json, null, "  ");
+      // This output isn't really needed anymore as we are writing the file inside this script, but just for future reference:
+      core.setOutput("json", text);
+      fs.writeFile(core.getInput("file"), text, function (err) { if (err) { throw err; } });
+      successful = true;
+    } else {
+      throw new Error("Empty JSON Filter!");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  core.setOutput("successful", successful);
+})();
