0aa3e3dae5e224666b78e5dd3338ac2997b558aa
infyscroll <70573082+infyscroll@users.noreply.github.com>
2022-07-13 01:49:38 -0400

    Add files via upload

--- .github/actions/fetch-json-write-file/dist/index.js
+++ .github/actions/fetch-json-write-file/dist/index.js
@@ -8518,8 +8518,9 @@ __nccwpck_require__.r(__webpack_exports__);
  * To compile this script into dist/index.js with all the necessary libraries:
  * 1. install vercel/ncc: npm i -g @vercel/ncc
  * 2. build index.js: ncc build index.js --license licenses.txt
- * @see https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github 
+ * @see https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github
  */
+
 const core = __nccwpck_require__(1006);
 const github = __nccwpck_require__(6738);
 const fs = __nccwpck_require__(7147);
@@ -8527,29 +8528,43 @@ const fs = __nccwpck_require__(7147);
 
 
 (async () => {
-  console.log("execute");
-  let successful = false;
-  try {
-    const url = core.getInput("is-url");
-    const file = core.getInput("is-file");
-    const response = await node_fetch__WEBPACK_IMPORTED_MODULE_0___default()(url);
-    const json = await response.json();
-    const filter = json.filter(d => d.data && d.data.url);
-    if (filter && filter.length > 0) {
-      const text = JSON.stringify(json, null, "  ");
-      // This output isn't really needed anymore as we are writing the file inside this script, but just for future reference:
-      core.setOutput("json", text);
-      fs.writeFile(file, text, function (err) { if (err) { throw err; } });
-      successful = true;
-    } else {
-      throw new Error("Empty JSON Filter!");
+
+  /**
+   * Performs the workflow action: downloading the database as JSON and writing it to a file in the repository.
+   *
+   * @param database the database name (used for the inputs and filtering the database)
+   */
+  async function action(database) {
+    console.log("action() - database=" + database);
+    let successful = false;
+    try {
+      const url = core.getInput(database + "-url");
+      const response = await node_fetch__WEBPACK_IMPORTED_MODULE_0___default()(url);
+      const json = await response.json();
+      const filter = database === "ap" ? json.filter(d => d.data && d.data.url && d.data.nextLink && d.data.pageElement) : json.filter(d => d.data && d.data.url && d.data.action && d.data.append);
+      if (filter && filter.length > 0) {
+        const file = core.getInput(database + "-file");
+        const text = JSON.stringify(json, null, "  ");
+        // This output isn't really needed anymore as we are writing the file inside this script, but just for future reference:
+        // core.setOutput(database + "-json", text);
+        fs.writeFile(file, text, function (err) { if (err) { throw err; } });
+        successful = true;
+      } else {
+        throw new Error("empty database!");
+      }
+    } catch (e) {
+      console.log(e);
     }
-  } catch (e) {
-    console.log(e);
+    // This output also isn't needed anymore as we are doing the successful check inside this script when we check that filter.length isn't 0
+    core.setOutput(database + "-successful", successful);
+    console.log("action() - successful=" + successful);
   }
-  console.log("execute, successful=" + successful);
-  core.setOutput("successful", successful);
-})(); 
+
+  // Perform the actions for each database in successive order (await each fetch request just to be safe?)
+  await action("ap");
+  await action("is");
+
+})();
 })();
 
 module.exports = __webpack_exports__;
