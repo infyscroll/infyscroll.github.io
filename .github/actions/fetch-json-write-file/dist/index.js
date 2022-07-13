ce2d59b907da50a680ece7c8d29089d363088689
infyscroll <70573082+infyscroll@users.noreply.github.com>
2022-07-13 00:56:43 -0400

    Add files via upload

--- .github/actions/fetch-json-write-file/dist/index.js
+++ .github/actions/fetch-json-write-file/dist/index.js
@@ -8527,31 +8527,34 @@ const fs = __nccwpck_require__(7147);
 
 
 
-async function execute(database) {
-  console.log("execute() - database=" + database);
-  let successful = false;
-  try {
-    const url = core.getInput(database + "-url");
-    const response = await node_fetch__WEBPACK_IMPORTED_MODULE_0___default()(url);
-    const json = await response.json();
-    const filter = database === "ap" ? json.filter(d => d.data && d.data.url && d.data.nextLink && d.data.pageElement) : json.filter(d => d.data && d.data.url && d.data.action && d.data.append);
-    if (filter && filter.length > 0) {
-      const text = JSON.stringify(json, null, "  ");
-      // This output isn't really needed anymore as we are writing the file inside this script, but just for future reference:
-      core.setOutput(database + "-json", text);
-      fs.writeFile(core.getInput(database + "-file"), text, function (err) { if (err) { throw err; } });
-      successful = true;
-    } else {
-      throw new Error("Empty JSON Filter!");
-    }
-  } catch (e) {
-    console.log(e);
-  }
-  core.setOutput(database + "successful", successful);
-  console.log("execute() - successful=" + successful);
-}
-
 (async () => {
+
+  async function execute(database) {
+    console.log("execute() - database=" + database);
+    let successful = false;
+    try {
+      const url = core.getInput(database + "-url");
+      console.log("execute() - url=" + url);
+      const file = core.getInput(database + "-file");
+      console.log("execute() - file=" + file);
+      const response = await node_fetch__WEBPACK_IMPORTED_MODULE_0___default()(url);
+      const json = await response.json();
+      const filter = database === "ap" ? json.filter(d => d.data && d.data.url && d.data.nextLink && d.data.pageElement) : json.filter(d => d.data && d.data.url && d.data.action && d.data.append);
+      if (filter && filter.length > 0) {
+        const text = JSON.stringify(json, null, "  ");
+        // This output isn't really needed anymore as we are writing the file inside this script, but just for future reference:
+        // core.setOutput(database + "-json", text);
+        fs.writeFile(file, text, function (err) { if (err) { throw err; } });
+        successful = true;
+      } else {
+        throw new Error("Empty database!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    // core.setOutput(database + "successful", successful);
+    console.log("execute() - successful=" + successful);
+  }
 
   await execute("ap");
   await execute("is");
